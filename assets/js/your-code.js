bfe40a4544050571e26307b0f2ef7fc9b9604e51
Nick <nick.buscemi13@gmail.com>
2023-05-18 11:31:22 -0400

    finished

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,45 @@
 export function getAllDogs() {
     // Your code here
+    return fetch('/dogs')
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    return fetch('/dogs/2')
 }
 
 export function postNewDog() {
     // Your code here
+    const options = {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams({
+            name: "Fido",
+            age: "4"
+        })
+    };
+    return fetch("/dogs", options);
 }
 
 export function postNewDogV2(name, age) {
      // Your code here
+     const options = {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams({
+            name: name,
+            age: age
+        })
+    };
+    return fetch("/dogs", options);
 }
 
 export function deleteDog(id) {
       // Your code here
+      
+      const options = {
+        method: "POST",
+        headers: { "AUTH": "ckyut5wau0000jyv5bsrud90y" }
+      };
+      return fetch(`/dogs/${id}/delete`, options)
 }
